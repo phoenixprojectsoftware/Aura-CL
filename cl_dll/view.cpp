e487353818d24e3b293d08fdda256163c48edbff
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2021-06-19 19:40:43 +0100

    Remove viewroll
    After porting Source 2013's bob behaviour to Aura, viewroll now crashes the engine. It's logical to remove it.

--- cl_dll/view.cpp
+++ cl_dll/view.cpp
@@ -91,7 +91,6 @@ cvar_t* cl_bobcycle_max;
 cvar_t* cl_bobup;
 cvar_t* cl_waterdist;
 cvar_t* cl_chasedist;
-cvar_t* cl_viewroll;
 
 cvar_t* cl_viewmodel_ofs_right;
 cvar_t* cl_viewmodel_ofs_forward;
@@ -482,7 +481,7 @@ void V_CalcViewRoll(struct ref_params_s* pparams)
 	viewentity = gEngfuncs.GetEntityByIndex(pparams->viewentity);
 	if (!viewentity)
 		return;
-	if (cl_viewroll->value == 1) pparams->viewangles[ROLL] += V_CalcRoll(pparams->viewangles, pparams->simvel, cl_rollangle->value, cl_rollspeed->value) * 4;
+	// if (cl_viewroll->value == 1) pparams->viewangles[ROLL] += V_CalcRoll(pparams->viewangles, pparams->simvel, cl_rollangle->value, cl_rollspeed->value) * 4;
 
 	side = V_CalcRoll(viewentity->angles, pparams->simvel, pparams->movevars->rollangle, pparams->movevars->rollspeed);
 
@@ -1821,7 +1820,6 @@ void V_Init(void)
 
 	cl_waterdist = gEngfuncs.pfnRegisterVariable("cl_waterdist", "4", 0);
 	cl_chasedist = gEngfuncs.pfnRegisterVariable("cl_chasedist", "112", 0);
-	cl_viewroll = gEngfuncs.pfnRegisterVariable("cl_viewroll", "0", 0);
 	
 	cl_viewmodel_ofs_right = gEngfuncs.pfnRegisterVariable("cl_viewmodel_ofs_right", "0", FCVAR_ARCHIVE); // x = right
 	cl_viewmodel_ofs_forward = gEngfuncs.pfnRegisterVariable("cl_viewmodel_ofs_forward", "0", FCVAR_ARCHIVE); // y = forward
