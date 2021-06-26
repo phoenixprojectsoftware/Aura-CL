3d07e58843acb9def39a0e76e193af83cb2a0a59
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2021-06-26 16:42:01 +0100

    New ver

--- cl_dll/hud_watermark.cpp
+++ cl_dll/hud_watermark.cpp
@@ -36,8 +36,8 @@ int CHudWatermark::Draw(float time)
 	UnpackRGB(r, g, b, gHUD.m_iDefaultHUDColor);
 
 	gEngfuncs.pfnDrawString(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight, "Aura client build "  __TIMESTAMP__, r, g, b);
-	gEngfuncs.pfnDrawString(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight * 2, "Half-Life: Zombies Ate My Neighbours Multiplayer 2.6-PPT1", r, g, b);
-	gEngfuncs.pfnDrawString(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight * 3, "Season 6", r, g, b);
+	gEngfuncs.pfnDrawString(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight * 2, "Half-Life: Zombies Ate My Neighbours Multiplayer 2.6-PPT2", r, g, b);
+	gEngfuncs.pfnDrawString(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight * 3, "Season 6: HECU", r, g, b);
 
 	if (update_is_available)
 		gEngfuncs.pfnDrawString(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight / 2 * 7, " ", r, g, b);
