ace8c885be5db704e281c280834a74a1ba57d220
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2021-10-31 19:47:07 +0000

    Changes to the HUD watermarks

--- cl_dll/versioninfo.h
+++ cl_dll/versioninfo.h
@@ -2,8 +2,8 @@
 // If you're using Aura for your own mod, change the values for the zamnhlmp definers.
 
 // These are in the order they appear in PrintVersion()
-#define auraVersion "v2.0"
-#define season "Season 6: HECU"
+#define auraVersion "v2.1"
+#define season "Season 7: Black Ops"
 #define zamnhlmpVersion "2.7-DEV"
 #define Authors "The Phoenix Project Software"
 #define auraCL "https://github.com/phoenixprojectsoftware/Aura-CL"
