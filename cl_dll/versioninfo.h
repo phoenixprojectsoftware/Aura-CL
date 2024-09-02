d8ba54ac85531252a01da9d79e29d10952f63912
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2024-09-03 00:04:23 +0100

    Update versioninfo.h

--- cl_dll/versioninfo.h
+++ cl_dll/versioninfo.h
@@ -2,8 +2,8 @@
 // If you're using Aura for your own mod, change the values for the zamnhlmp definers.
 
 // These are in the order they appear in PrintVersion()
-#define auraVersion "v2.2"
-#define season "Season 8: Black Mesa Inbound"
+#define auraVersion "v2.3"
+#define season "Season 9: White Sands"
 #define Authors "The Phoenix Project Software"
 #define auraCL "https://github.com/phoenixprojectsoftware/Aura-CL"
 #define auraSE "https://github.com/phoenixprojectsoftware/Aura-SE"
