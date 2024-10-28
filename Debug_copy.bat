932de65f2cbdf2a1ad8d8e1f173f198f2981ffa5
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2024-10-28 23:41:02 +0000

    Update Debug_copy.bat
    Co-Authored-By: Joël Troch <JoelTroch@users.noreply.github.com>

--- Debug_copy.bat
+++ Debug_copy.bat
@@ -1,3 +1,3 @@
 @echo off
-xcopy /f /y "build\Debug" "C:\Program Files (x86)\Steam\steamapps\common\Half-Life\zamnhlmp_dev\cl_dlls"
+xcopy /f /y "build\Debug\" "C:\Program Files (x86)\Steam\steamapps\common\Half-Life\zamnhlmp_dev\cl_dlls"
 pause
