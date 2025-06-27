a7b231a0bfe1bb2c62355f598ca5424f2f07bdff
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2025-06-27 22:24:10 +0100

    Fix assignment of cl_cross_low variable
    Replaces the commented-out CVAR_CREATE assignment with direct assignment to crosshair_low for cl_cross_low, ensuring correct initialization.

--- cl_dll/hud_crosshairs.cpp
+++ cl_dll/hud_crosshairs.cpp
@@ -10,7 +10,7 @@ int CHudCrosshairs::Init()
 	m_iFlags = HUD_ACTIVE;
 
 	cl_cross =               CVAR_CREATE("cl_cross", "0", FCVAR_ARCHIVE);
-	cl_cross_low =			 crosshair_low;//CVAR_CREATE("cl_cross_low", "1", FCVAR_ARCHIVE);
+	cl_cross_low =			 crosshair_low;
 	cl_cross_color =         CVAR_CREATE("cl_cross_color", "0 255 0", FCVAR_ARCHIVE);
 	cl_cross_alpha =         CVAR_CREATE("cl_cross_alpha", "200", FCVAR_ARCHIVE);
 	cl_cross_thickness =     CVAR_CREATE("cl_cross_thickness", "2", FCVAR_ARCHIVE);
