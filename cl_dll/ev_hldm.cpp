39a15ff4f3323a7903909a5a8c71b8637311894c
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2025-06-30 13:32:12 +0100

    Fix Pipe wrench animations not always playing and sounds being played multiple times
    Co-Authored-By: Sam Vanheer <5501383+samvanheer@users.noreply.github.com>

--- cl_dll/ev_hldm.cpp
+++ cl_dll/ev_hldm.cpp
@@ -2131,6 +2131,8 @@ void EV_Knife(event_args_t* args)
 //======================
 //	PIPE WRENCH START
 //======================
+int g_iClub;
+
 //Only predict the miss sounds, hit sounds are still played 
 //server side, so players don't get the wrong idea.
 void EV_Pipewrench(event_args_t* args)
@@ -2138,40 +2140,64 @@ void EV_Pipewrench(event_args_t* args)
 	const int idx = args->entindex;
 	Vector origin = args->origin;
 	const int iBigSwing = args->bparam1;
+	const int hitSomething = args->bparam2;
 
+	if (!EV_IsLocal(idx))
+	{
+		return;
+	}
+
+	//TODO: hits aren't handled here because the server is already doing those
+	//TODO: vanilla Op4 always plays miss sounds, causing duplication
 	//Play Swing sound
 	if (iBigSwing)
 	{
-		gEngfuncs.pEventAPI->EV_PlaySound(idx, origin, CHAN_WEAPON, "weapons/pwrench_big_miss.wav", 1, ATTN_NORM, 0, PITCH_NORM);
-		if (steam_vibrate_enabled->value >= 1)
-			gHUD.StartControllerVibration(0, 30000, 0, 30000, 0.2f);
-	}
-	else
-	{
-		switch (gEngfuncs.pfnRandomLong(0, 1))
+		if (hitSomething)
 		{
-		case 0: gEngfuncs.pEventAPI->EV_PlaySound(idx, origin, CHAN_WEAPON, "weapons/pwrench_miss1.wav", 1, ATTN_NORM, 0, PITCH_NORM); break;
-		case 1: gEngfuncs.pEventAPI->EV_PlaySound(idx, origin, CHAN_WEAPON, "weapons/pwrench_miss2.wav", 1, ATTN_NORM, 0, PITCH_NORM); break;
-		}
-	}
-
-	if (EV_IsLocal(idx))
-	{
-		if (iBigSwing)
-		{
-			Punch(2, 0, 0);
-			gEngfuncs.pEventAPI->EV_WeaponAnimation(PIPEWRENCH_BIG_SWING_MISS, 1);
+			//gEngfuncs.pEventAPI->EV_WeaponAnimation(PIPEWRENCH_BIG_SWING_HIT, 0);
+			if (steam_vibrate_enabled->value >= 1)
+				gHUD.StartControllerVibration(0, 30000, 0, 30000, 0.2f);
 		}
 		else
 		{
-			switch ((g_iSwing++) % 3)
+			gEngfuncs.pEventAPI->EV_WeaponAnimation(PIPEWRENCH_BIG_SWING_MISS, 0);
+
+			gEngfuncs.pEventAPI->EV_PlaySound(idx, origin, CHAN_WEAPON, "weapons/pwrench_big_miss.wav", 1, ATTN_NORM, 0, PITCH_NORM);
+		}
+	}
+	else
+	{
+		if (hitSomething)
+		{
+			// This is done SERVERSIDE
+			/*
+			switch (g_iClub % 3)
 			{
 			case 0:
-				gEngfuncs.pEventAPI->EV_WeaponAnimation(PIPEWRENCH_ATTACK1MISS, 1); break;
+				gEngfuncs.pEventAPI->EV_WeaponAnimation(PIPEWRENCH_ATTACK1HIT, 0); break;
 			case 1:
-				gEngfuncs.pEventAPI->EV_WeaponAnimation(PIPEWRENCH_ATTACK2MISS, 1); break;
+				gEngfuncs.pEventAPI->EV_WeaponAnimation(PIPEWRENCH_ATTACK2HIT, 0); break;
 			case 2:
-				gEngfuncs.pEventAPI->EV_WeaponAnimation(PIPEWRENCH_ATTACK3MISS, 1); break;
+				gEngfuncs.pEventAPI->EV_WeaponAnimation(PIPEWRENCH_ATTACK2HIT, 0); break;
+			}
+			*/
+		}
+		else
+		{
+			switch (g_iClub % 3)
+			{
+			case 0:
+				gEngfuncs.pEventAPI->EV_WeaponAnimation(PIPEWRENCH_ATTACK1MISS, 0); break;
+			case 1:
+				gEngfuncs.pEventAPI->EV_WeaponAnimation(PIPEWRENCH_ATTACK2MISS, 0); break;
+			case 2:
+				gEngfuncs.pEventAPI->EV_WeaponAnimation(PIPEWRENCH_ATTACK3MISS, 0); break;
+			}
+
+			switch (g_iClub % 2)
+			{
+			case 0: gEngfuncs.pEventAPI->EV_PlaySound(idx, origin, CHAN_WEAPON, "weapons/pwrench_miss1.wav", 1, ATTN_NORM, 0, PITCH_NORM); break;
+			case 1: gEngfuncs.pEventAPI->EV_PlaySound(idx, origin, CHAN_WEAPON, "weapons/pwrench_miss2.wav", 1, ATTN_NORM, 0, PITCH_NORM); break;
 			}
 		}
 	}
