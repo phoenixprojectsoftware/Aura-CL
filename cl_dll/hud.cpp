1ba7c27a04b324713840393db23d851bcceb5496
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2025-07-12 17:17:15 +0100

    Revert hardcoded default_fov to 90

--- cl_dll/hud.cpp
+++ cl_dll/hud.cpp
@@ -644,7 +644,7 @@ void CHud :: Init( void )
 	m_iTargetFOV = 0;
 
 	CVAR_CREATE( "zoom_sensitivity_ratio", "1.2", 0 );
-	default_fov = CVAR_CREATE( "default_fov", "110", FCVAR_ARCHIVE ); // I have set the hard-coded default to 110. Why would you want to use 90 on a widescreen display?
+	default_fov = CVAR_CREATE( "default_fov", "90", FCVAR_ARCHIVE );
 	m_pCvarStealMouse = CVAR_CREATE( "hud_capturemouse", "1", FCVAR_ARCHIVE );
 	m_pCvarDraw = CVAR_CREATE( "hud_draw", "1", FCVAR_ARCHIVE );
 	m_pCvarDrawDeathNoticesAlways = CVAR_CREATE( "cl_draw_deathnotices_always", "0", FCVAR_ARCHIVE );
