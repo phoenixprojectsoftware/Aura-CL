b5a7f9a4143ad302cac1a9305f34bb51657a10da
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2025-07-19 17:15:21 +0100

    Add CDialogServerPassword stub
    Co-Authored-By: Johan Ehrendahl <jonnyboy0719@gmail.com>

---
+++ cl_dll/gameui/serverbrowser/dialogs/CDialogServerPassword.cpp
@@ -0,0 +1,6 @@
+// ============== Copyright (c) 2025 Monochrome Games ============== \\
+
+#include "CDialogServerPassword.h"
+
+// INFO: Should we use this CPP file?
+// At the moment, CDialogServerPassword is not used.
