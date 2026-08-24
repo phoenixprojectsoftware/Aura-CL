004c5f4d40d9a1095ce348a39c63c389f0b78abd
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2026-08-24 18:36:37 +0100

    Remove Berony from Phoenix Steam ID list

--- cl_dll/zamnhlmp_achievements.h
+++ cl_dll/zamnhlmp_achievements.h
@@ -22,7 +22,6 @@ static const char* g_PhoenixSteamIDs[] = {
 	"0:1:54795934", // jan
 	"0:1:63945665", // rafael
 	"0:1:484919737", // frostlander
-	"0:0:457283837", // berony
 	"0:1:516167454", // hitoshii
 	"0:0:421607015", // galexion
 	"0:0:105253777", // finger
